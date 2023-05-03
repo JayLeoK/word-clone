7ef460b45019ea079cf6743b10f5a9c181204769
Jaydon Krooss <jkrooss@gmail.com>
2023-05-03 13:07:23 -0400

    exercise5 game completion, banner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-import { sample } from "../../utils";
+import { range, sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults";
+import EndBanner from "../EndBanner";
 import { checkGuess } from "../../game-helpers";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
@@ -12,16 +15,32 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [gameState, setGameState] = React.useState("running");
 
   const submitGuess = (value) => {
-    const result = checkGuess(value, answer);
-    setGuesses([...guesses, result]);
+    if (gameState !== "running") {
+      return;
+    }
+    const checkResult = checkGuess(value, answer);
+    const nextGuesses = [...guesses, checkResult];
+    setGuesses(nextGuesses);
+
+    //set win condition
+    if (value == answer) {
+      setGameState("win");
+    }
+    //completion without success
+    else if (nextGuesses.length === NUM_OF_GUESSES_ALLOWED) {
+      setGameState("loss");
+    }
   };
 
   return (
     <>
-      <GuessResults length={guesses.length} guesses={guesses} />
-
+      <GuessResults guesses={guesses} />
+      {gameState !== "running" && (
+        <EndBanner gameState={gameState} guesses={guesses} answer={answer} />
+      )}
       <GuessInput submitGuess={submitGuess}></GuessInput>
     </>
   );
